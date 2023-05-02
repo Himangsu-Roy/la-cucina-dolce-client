7ca6089f8f18040cd115c37c9c5283bde0323446
himangsu roy <himur98@gmail.com>
2023-05-02 23:22:27 +0600

    Popular categories section added

--- src/components/PopularCategories/PopularCategoris.jsx
+++ src/components/PopularCategories/PopularCategoris.jsx
@@ -3,118 +3,118 @@ import React from "react";
 const PopularCategoris = () => {
   return (
     <div>
-      <h1>Popular Categories</h1>
-      <div>
-        <div class="flex">
-          <div class="col-lg-2 col-md-4 col-4">
-            <figure class="my-3 text-center tstbite-card">
+      <div className="mt-[40px] mb-8">
+        <h1 className="text-[35px] font-[700] ml-5 mb-2">Popular Categories</h1>
+        <div className="flex flex-wrap justify-around">
+          <div className="">
+            <figure className="my-3 text-center tstbite-card">
               <a
-                href="category.html"
-                class="tstbite-animation stretched-link rounded-full"
+                href="#"
+                className="tstbite-animation stretched-link rounded-full"
               >
                 <img
                   src="https://fabrx.co/preview/tastebite/assets/images/menus/menu8.png"
-                  class="rounded-circle"
+                  className="rounded-circle"
                   alt="Menu"
                 />
               </a>
-              <figcaption class="mt-2">
-                <a href="category.html" class="tstbite-category-title">
+              <figcaption className="mt-2">
+                <a href="#" className="tstbite-category-title">
                   Pasta
                 </a>
               </figcaption>
             </figure>
           </div>
-          <div class="col-lg-2 col-md-4 col-4">
-            <figure class="my-3 text-center tstbite-card">
+          <div className="col-lg-2 col-md-4 col-4">
+            <figure className="my-3 text-center tstbite-card">
               <a
-                href="category.html"
-                class="tstbite-animation stretched-link rounded-circle"
+                href="#"
+                className="tstbite-animation stretched-link rounded-circle"
               >
                 <img
                   src="https://fabrx.co/preview/tastebite/assets/images/menus/menu9.png"
-                  class="rounded-circle"
+                  className="rounded-circle"
                   alt="Menu"
                 />
               </a>
-              <figcaption class="mt-2">
-                <a href="category.html" class="tstbite-category-title">
+              <figcaption className="mt-2">
+                <a href="#" className="tstbite-category-title">
                   Pizza
                 </a>
               </figcaption>
             </figure>
           </div>
-          <div class="col-lg-2 col-md-4 col-4">
-            <figure class="my-3 text-center tstbite-card">
+          <div className="col-lg-2 col-md-4 col-4">
+            <figure className="my-3 text-center tstbite-card">
               <a
-                href="category.html"
-                class="tstbite-animation stretched-link rounded-circle"
+                href="#"
+                className="tstbite-animation stretched-link rounded-circle"
               >
                 <img
-                  src="assets/images/menus/menu10.png"
-                  class="rounded-circle"
+                  src="https://fabrx.co/preview/tastebite/assets/images/menus/menu10.png"
+                  className="rounded-circle"
                   alt="Menu"
                 />
               </a>
-              <figcaption class="mt-2">
-                <a href="category.html" class="tstbite-category-title">
+              <figcaption className="mt-2">
+                <a href="#" className="tstbite-category-title">
                   Vegan
                 </a>
               </figcaption>
             </figure>
           </div>
-          <div class="col-lg-2 col-md-4 col-4">
-            <figure class="my-3 text-center tstbite-card">
+          <div className="col-lg-2 col-md-4 col-4">
+            <figure className="my-3 text-center tstbite-card">
               <a
-                href="category.html"
-                class="tstbite-animation stretched-link rounded-circle"
+                href="#"
+                className="tstbite-animation stretched-link rounded-circle"
               >
                 <img
-                  src="assets/images/menus/menu11.png"
-                  class="rounded-circle"
+                  src="https://fabrx.co/preview/tastebite/assets/images/menus/menu11.png"
+                  className="rounded-circle"
                   alt="Menu"
                 />
               </a>
-              <figcaption class="mt-2">
-                <a href="category.html" class="tstbite-category-title">
+              <figcaption className="mt-2">
+                <a href="#" className="tstbite-category-title">
                   Desserts
                 </a>
               </figcaption>
             </figure>
           </div>
-          <div class="col-lg-2 col-md-4 col-4">
-            <figure class="my-3 text-center tstbite-card">
+          <div className="col-lg-2 col-md-4 col-4">
+            <figure className="my-3 text-center tstbite-card">
               <a
-                href="category.html"
-                class="tstbite-animation stretched-link rounded-circle"
+                href="#"
+                className="tstbite-animation stretched-link rounded-circle"
               >
                 <img
-                  src="assets/images/menus/menu12.png"
-                  class="rounded-circle"
+                  src="https://fabrx.co/preview/tastebite/assets/images/menus/menu12.png"
+                  className="rounded-circle"
                   alt="Menu"
                 />
               </a>
-              <figcaption class="mt-2">
-                <a href="category.html" class="tstbite-category-title">
+              <figcaption className="mt-2">
+                <a href="#" className="tstbite-category-title">
                   Smoothies
                 </a>
               </figcaption>
             </figure>
           </div>
-          <div class="col-lg-2 col-md-4 col-4">
-            <figure class="my-3 text-center tstbite-card">
+          <div className="col-lg-2 col-md-4 col-4">
+            <figure className="my-3 text-center tstbite-card">
               <a
-                href="category.html"
-                class="tstbite-animation stretched-link rounded-circle"
+                href="#"
+                className="tstbite-animation stretched-link rounded-circle"
               >
                 <img
-                  src="assets/images/menus/menu13.png"
-                  class="rounded-circle"
+                  src="https://fabrx.co/preview/tastebite/assets/images/menus/menu13.png"
+                  className="rounded-circle"
                   alt="Menu"
                 />
               </a>
-              <figcaption class="mt-2">
-                <a href="category.html" class="tstbite-category-title">
+              <figcaption className="mt-2">
+                <a href="#" className="tstbite-category-title">
                   Breakfast
                 </a>
               </figcaption>
